d209bfb5342f737b77718de499e507effc9aaf05
Sophia Roamcnhuk <romanchuk@ucu.edu.ua>
2019-12-20 16:53:39 +0200

    Initial commit

---
+++ src/main/java/ua/edu/ucu/stream/AsIntStream.java
@@ -0,0 +1,143 @@
+package ua.edu.ucu.stream;
+
+import ua.edu.ucu.function.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.lang.*;
+
+
+public class AsIntStream implements IntStream {
+
+    private ArrayList<Integer> stream;
+
+    private AsIntStream(int... values) {
+        this.stream = new ArrayList<>();
+        for (int i = 0; i < values.length; i++) {
+            this.stream.add(values[i]);
+        }
+    }
+
+    private AsIntStream(ArrayList<Integer> stream) {
+        this.stream = stream;
+    }
+
+    public static IntStream of(int... values) {
+        IntStream intStream = new AsIntStream(values);
+        return intStream;
+    }
+
+    @Override
+    public Double average() {
+        Double count = (double) this.count();
+        return this.sum() / count;
+    }
+
+    @Override
+    public Integer max() {
+        Integer max = Integer.MIN_VALUE;
+        for (Integer val : this.stream) {
+            if (val > max) {
+                max = val;
+            }
+        }
+        return max;
+    }
+
+    @Override
+    public Integer min() {
+        Integer min = Integer.MAX_VALUE;
+        for (Integer val : this.stream) {
+            if (val < min) {
+                min = val;
+            }
+        }
+        return min;
+    }
+
+    @Override
+    public long count() {
+        return this.stream.size();
+    }
+
+    @Override
+    public Integer sum() {
+        Integer sum = 0;
+        for (Integer val : this.stream) {
+            sum += val;
+        }
+        return sum;
+    }
+
+    @Override
+    public IntStream filter(IntPredicate predicate) {
+        int[] arr = this.toArray();
+        int[] res = new int[arr.length];
+        //перетворюєм потік на масив
+        int k = 0;
+        //індексація результ масиву
+        for (int i = 0; i < arr.length; i++) {
+            if (predicate.test(arr[i])) {
+
+                res[k] = arr[i];
+                k++;
+            }
+        }
+        return AsIntStream.of(Arrays.copyOf(res, k));
+        //відрізаєм непотрібне
+    }
+
+    @Override
+    public void forEach(IntConsumer action) {
+        int[] arr = this.toArray();
+        for (int i = 0; i < arr.length; i++) {
+            action.accept(arr[i]);
+        }
+    }
+
+    @Override
+    public IntStream map(IntUnaryOperator mapper) {
+        int[] arr = this.toArray();
+        int[] res = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = mapper.apply(arr[i]);
+        }
+
+        return AsIntStream.of(res);
+    }
+
+    @Override
+    public IntStream flatMap(IntToIntStreamFunction func) {
+        int[] arr = this.toArray();
+        int len = func.applyAsIntStream(arr[0]).toArray().length;
+        int[] preRes = new int[len];
+        int[] res = new int[arr.length * len];
+        for (int i = 0; i < arr.length; i++) {
+            preRes = func.applyAsIntStream(arr[i]).toArray();
+            for (int j = 0; j < preRes.length; j++) {
+                res[len * i + j] = preRes[j];
+            }
+        }
+
+        return AsIntStream.of(res);
+    }
+
+    @Override
+    public int reduce(int identity, IntBinaryOperator op) {
+        int[] arr = this.toArray();
+        int result = identity;
+        for (int i = 0; i < arr.length; i++) {
+            result = op.apply(result, arr[i]);
+        }
+        return result;
+    }
+
+    @Override
+    public int[] toArray() {
+        int[] arr = new int[this.stream.size()];
+        for (int i = 0; i < this.stream.size(); i++) {
+            arr[i] = this.stream.get(i);
+        }
+        return arr;
+    }
+}
---
+++ src/test/java/ua/edu/ucu/StreamAppTest.java
@@ -0,0 +1,112 @@
+package ua.edu.ucu;
+
+import ua.edu.ucu.stream.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author andrii
+ */
+public class StreamAppTest {
+    
+    private IntStream intStream;
+
+    @Before
+    public void init() {
+        int[] intArr = {-1, 0, 1, 2, 3};
+        intStream = AsIntStream.of(intArr);
+    }
+    
+    @Test
+    public void testStreamOperations() {
+        System.out.println("streamOperations");
+        int expResult = 42;
+        int result = StreamApp.streamOperations(intStream);
+        assertEquals(expResult, result);        
+    }
+
+    @Test
+    public void testStreamToArray() {
+        System.out.println("streamToArray");
+        int[] expResult = {-1, 0, 1, 2, 3};
+        int[] result = StreamApp.streamToArray(intStream);
+        assertArrayEquals(expResult, result);        
+    }
+
+    @Test
+    public void testStreamForEach() {
+        System.out.println("streamForEach");
+        String expResult = "-10123";
+        String result = StreamApp.streamForEach(intStream);
+        assertEquals(expResult, result);        
+    }
+
+    @Test
+    public void testStreamAverage() {
+        System.out.println("average");
+        double expResult = 1.0;
+        double result = intStream.average();
+        assertEquals(expResult, (Object) result);
+    }
+
+    @Test
+    public void testStreamMax() {
+        System.out.println("max");
+        double expResult = 3;
+        double result = intStream.max();
+        assertEquals(expResult, (Object) result);
+    }
+
+    @Test
+    public void testStreamMin() {
+        System.out.println("min");
+        double expResult = -1;
+        double result = intStream.min();
+        assertEquals(expResult, (Object) result);
+    }
+
+    @Test
+    public void testStreamCount() {
+        System.out.println("count");
+        double expResult = 5;
+        double result = intStream.count();
+        assertEquals(expResult, (Object) result);
+    }
+
+    @Test
+    public void testStreamSum() {
+        System.out.println("sum");
+        double expResult = 5;
+        double result = intStream.sum();
+        assertEquals(expResult, (Object) result);
+    }
+
+    @Test
+    public void testStreamFilter() {
+        System.out.println("filter");
+        IntStream expResult = AsIntStream.of(2, 3);
+        IntStream output = AsIntStream.of(-1, 0, 1, 2, 3);
+        IntStream result = output.filter(x -> x > 1);
+        assertArrayEquals(expResult.toArray(), result.toArray());
+    }
+
+    @Test
+    public void testStreamMap() {
+        System.out.println("map");
+        IntStream expResult = AsIntStream.of(2, 4, 6);
+        IntStream output = AsIntStream.of(1, 2, 3);
+        IntStream result = output.map(x -> x * 2);
+        assertArrayEquals(expResult.toArray(), result.toArray());
+    }
+
+    @Test
+    public void testStreamFlatMap() {
+        System.out.println("flatMap");
+        IntStream expResult = AsIntStream.of(-1, 1, 3, 2, 4, 6, 7, 9, 11);
+        IntStream output = AsIntStream.of(1, 4, 9);
+        IntStream result = output.flatMap(x -> AsIntStream.of(x - 2, x, x + 2));
+        assertArrayEquals(expResult.toArray(), result.toArray());
+    }
+}
